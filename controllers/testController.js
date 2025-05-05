0918b09cafca4e1c7652ac1ea52b1227a3fecb36
Oluwaseun Akande <akandeseun44@gmail.com>
2025-05-05 16:30:47 +0100

    feat: setup for templates

--- controllers/testController.js
+++ controllers/testController.js
@@ -1,14 +1,15 @@
 const newman = require('newman');
+const path = require('path');
+const fs = require('fs');
 
 const executeTest = async (req, res) => {
-  // Check if a file was uploaded
   if (!req.file) {
     return res.status(400).send('No collection file uploaded.');
   }
 
   let collectionJson;
   try {
-    // Read the file buffer and parse it as JSON
+    // parse file as JSON
     const fileContent = req.file.buffer.toString('utf8');
     collectionJson = JSON.parse(fileContent);
   } catch (parseError) {
@@ -23,7 +24,6 @@ const executeTest = async (req, res) => {
 
   newman.run(
     {
-      // Use the parsed JSON from the uploaded file
       collection: collectionJson,
       reporters: ['cli', 'htmlextra'],
       reporter: {
@@ -33,15 +33,14 @@ const executeTest = async (req, res) => {
       },
     },
     function (err, summary) {
-      // Added summary parameter
       if (err) {
         console.error('Newman run error:', err);
-        // Send an error response
         return res.status(500).json({ message: 'Collection run failed.', error: err });
       }
+
       console.log('collection run complete!');
-      // Send a success response
       console.log('stats', summary);
+
       res.status(200).json({
         message: 'Collection run complete!',
         stats: summary.run.stats,
@@ -50,6 +49,4 @@ const executeTest = async (req, res) => {
     },
   );
 };
-
-// Make sure to export the function if not already done elsewhere
 module.exports = { executeTest };
